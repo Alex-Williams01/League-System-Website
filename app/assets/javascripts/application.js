f695282018d12646466e9123b8802bdf77919ae3
Alex Williams <aw01121@surrey.ac.uk>
2020-12-12 21:22:06 +0000

    Simple Home Page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,30 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+function SetFontSize(val) {
+ 	$("*").each(function(i,e){e.style.fontSize = val});
+ }
+
+ function ToggleText(elem) {
+ 	$('#'+elem.id+"p").toggle();
+ }
+
+ $(document).ready(function() {
+
+ 	$('#contactp,#fixturesp,#teamsp,#unip,#accessibilityp').hide();
+ })
+
+ /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+ function openNav() {
+   document.getElementById("mySidebar").style.width = "250px";
+   document.getElementById("main").style.marginLeft = "250px";
+   document.getElementById("main_content").style.marginLeft = "250px";
+ }
+
+ /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+ function closeNav() {
+   document.getElementById("mySidebar").style.width = "0";
+   document.getElementById("main").style.marginLeft = "0";
+   document.getElementById("main_content").style.marginLeft = "0";
+ }
