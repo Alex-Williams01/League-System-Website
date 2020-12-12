b164560c05d6874ea3ee28479820754157f36366
Alex Williams <aw01121@surrey.ac.uk>
2020-12-12 21:27:33 +0000

    Added home testing

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_url
-    assert_select 'title', 'Home'
+    assert_select 'h2', 'Home'
+    assert_select 'p#welcome', 'Welcome to the National University League. Here you can access fixtures, league table, players\' information and team information. This is the home page where you can find details on all the pages and their functionality'
     assert_response :success
   end
 
@@ -13,7 +14,7 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get about" do
-    get home_about_url
+    get about_url
     assert_response :success
   end
 
