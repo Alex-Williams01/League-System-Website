17d47ff446284c8014806c366e3e7f21d840f4cf
Alex Williams <aw01121@surrey.ac.uk>
2020-12-12 21:48:41 +0000

    Added contact form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  ALERTS = {
+     :notice => "alert alert-info",
+     :success => "alert alert-success",
+     :error => "alert alert-danger",
+     :alert => "alert alert-warning",
+   }
+
+   def flash_class(level)
+     ALERTS[level.to_sym]
+   end
 end
