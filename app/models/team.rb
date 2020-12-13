21381d88928ac85f0d29f35c7898f0262b245228
Alex Williams <aw01121@surrey.ac.uk>
2020-12-13 13:17:10 +0000

    Added Match scaffold

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,6 +3,8 @@ class Team < ApplicationRecord
   has_many :players, dependent: :destroy
   #defines that each team belongs to a university
   belongs_to :university
+  #defines that each team has and belongs to many matches
+  has_and_belongs_to_many :matches
   #validates each field exists
   validates :name, :points, :university, presence: true
 end
