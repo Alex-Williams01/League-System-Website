2692394da84542c1cf9f981afbb973abe2437446
Alex Williams <aw01121@surrey.ac.uk>
2020-12-13 11:55:12 +0000

    Linked Player to Team and created tests

--- test/models/player_test.rb
+++ test/models/player_test.rb
@@ -4,4 +4,23 @@ class PlayerTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  setup do
+    @team = teams(:one)
+  end
+
+  test 'should not save empty player' do
+    player = Player.new
+    player.save
+    refute player.valid?
+  end
+
+  test 'should save valid player' do
+    player = Player.new
+    player.name = "myname"
+    player.playtime = 0.0
+    player.kd = 1.3
+    player.team = @team
+    player.save
+    assert player.valid?
+  end
 end
